1fa4066714e7b694bd5be63ec05311e8a213b8a2
Kyle Shores <kyle.shores44@gmail.com>
2020-05-20 21:33:35 -0500

    scaffolding AI

---
+++ handwritingAI.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class NeuralHandwritingNet:
+
+    def __init__():
+        pass
+
+    def backward_propogation():
+        pass
+
+    def feedforward():
+        pass
+
+if __name__ == '__main__':
+    ai = NeuralHandwritingNet()
