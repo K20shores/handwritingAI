1814bb0a5c96fa5af8960aaeee9d070648f85a57
Kyle Shores <kyle.shores44@gmail.com>
2020-05-20 22:01:15 -0500

    adding readme and scaffolding file reader

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # handwritingAI
 Self-made Neural Network to classify handwritten numbers
+
+# Setup environment
+`python3 -m venv .`
+`pip install -r requirements.txt`
+
+# Run
+Modify `make_graph.py` and then run `python3 make_graph.py`
--- handwritingAI.py
+++ handwritingAI.py
@@ -1,15 +1,26 @@
 import numpy as np
 
+class DataReader:
+    def __init__(self, fileName):
+        self.fileName = fileName
+
+    def read(self):
+        pass
+
 class NeuralHandwritingNet:
-
-
     def __init__(self):
         pass
 
-    def backward_propogation(self):
+    def train(self):
         pass
 
-    def feedforward(self):
+    def predict(self):
+        pass
+
+    def __backward_propogation(self):
+        pass
+
+    def __feedforward(self):
         pass
 
 if __name__ == '__main__':
