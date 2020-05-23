b0ed40daea5ca9ddabf8febd81acba2b563ec8cc
Kyle Shores <kyle.shores44@gmail.com>
2020-05-23 00:20:20 -0500

    base classing data reader, the ai now runs but may not be correct

--- handwritingAI.py
+++ handwritingAI.py
@@ -1,11 +1,31 @@
 import numpy as np
 import struct
-import math
 from tqdm import tqdm
 
-class LabelDataReader:
-    def __init__(self, fileName):
-        self.fileName = fileName
+class DataReader:
+    def __init__(self, file_name, magic_number):
+        self.file_name = file_name
+        self.magic_number = magic_number
+        with open(self.file_name, 'rb') as f:
+            self.file_contents = f.read()
+        self.offset = 0
+
+    def read_header(self, fmt):
+        sz = struct.calcsize(fmt)
+        data = struct.unpack(fmt, self.file_contents[self.offset:self.offset+sz])
+        self.offset += sz
+        return data
+
+    def check_magic_number(self, number):
+        if self.magic_number != number:
+            raise Exception("Invalid magic number. Make sure you are reading the number in big-endian format")
+
+    def unpack_file_contents(self):
+        return list(struct.iter_unpack(">B", self.file_contents[self.offset:]))
+
+class LabelDataReader(DataReader):
+    def __init__(self, file_name):
+        super().__init__(file_name, 2049)
 
     def read(self):
         """ Read the data format. Returns a list of labels [0-9]
@@ -13,37 +33,21 @@ class LabelDataReader:
         Format is from MNIST http://yann.lecun.com/exdb/mnist/
         """
 
-        labels = []
+        magic_number,data_length = self.read_header(">ii")
+        self.check_magic_number(magic_number)
 
-        with open(self.fileName, 'rb') as f:
-            fileContents = f.read()
+        print(f"Unpacking {data_length} label values")
+        labels = np.array(self.unpack_file_contents())
+        print(f"Finished unpacking labels")
 
-            offset = 0
-
-            # grab the magic number to do a sanity check and the data length
-            # see here for the format string, https://docs.python.org/3/library/struct.html#format-strings
-            magic_number,data_length = struct.unpack(">ii", fileContents[offset:offset+8])
-            offset += 8
-
-            # this number is specified to be 2049 on the MNIST site above
-            if magic_number != 2049:
-                raise Exception("Invalid magic number. Make sure you are reading the number in big-endian format")
-
-            print(f"Unpacking {data_length} label values")
-
-            for offset in range(offset, len(fileContents)):
-                label, = struct.unpack(">B", fileContents[offset:offset+1])
-                labels.append(label)
-
-            if (len(labels) != data_length):
-                raise Exception(f"Failed to read the correct number of data values. Read {len(labels)}, expected {data_length}")
-            print(f"Finished unpacking labels")
+        if (len(labels) != data_length):
+            raise Exception(f"Failed to read the correct number of data values. Read {len(labels)}, expected {data_length}")
 
         return labels
 
-class ImageDataReader:
-    def __init__(self, fileName):
-        self.fileName = fileName
+class ImageDataReader(DataReader):
+    def __init__(self, file_name):
+        super().__init__(file_name, 2051)
 
     def read(self):
         """ Read the data format. Returns a list of np 2-d arrays
@@ -51,80 +55,76 @@ class ImageDataReader:
         Format is from MNIST http://yann.lecun.com/exdb/mnist/
         """
 
-        images = []
+        magic_number,n_images, n_rows, n_columns = self.read_header(">iiii")
+        self.check_magic_number(magic_number)
 
-        with open(self.fileName, 'rb') as f:
-            fileContents = f.read()
+        print(f"Unpacking {n_images} images")
+        print(f"Image size (row, column): ({n_rows}, {n_columns})")
+        images = np.reshape(self.unpack_file_contents(), (n_images, n_columns * n_rows))
+        print(f"Finished unpacking images")
 
-            offset = 0
-
-            # grab the magic number to do a sanity check, the number of images, rows, and columns
-            # see here for the format string, https://docs.python.org/3/library/struct.html#format-strings
-            magic_number,n_images, n_rows, n_columns = struct.unpack(">iiii", fileContents[offset:offset+16])
-            offset += 16
-
-            # this number is specified to be 2049 on the MNIST site above
-            if magic_number != 2051:
-                raise Exception("Invalid magic number. Make sure you are reading the number in big-endian format")
-
-            print(f"Unpacking {n_images} images")
-            print(f"Image size (row, column): ({n_rows}, {n_columns})")
-
-            pixels = []
-            for offset in range(offset, len(fileContents)):
-                pixel, = struct.unpack(">B", fileContents[offset:offset+1])
-                pixels.append(pixel)
-
-            images = np.reshape(pixels, (n_images, n_columns * n_rows))
-
-
-            if (len(images) != n_images):
-                raise Exception(f"Failed to read the correct number of data values. Read {len(images)}, expected {n_images}")
-            print(f"Finished unpacking images")
+        if (len(images) != n_images):
+            raise Exception(f"Failed to read the correct number of data values. Read {len(images)}, expected {n_images}")
 
         return images
 
 class NeuralHandwritingNet:
-    def __init__(self):
-        self.weights1 = np.random.rand(784,4)
-        self.weights2 = np.random.rand(4,1)
-        self.output = np.zeros(1)
-
-    def train(self, x, y):
+    def __init__(self, x, y):
         self.input = x
         self.y = y
+        self.weights1 = np.random.rand(self.input.shape[1],4)
+        self.weights2 = np.random.rand(4,1)
+        self.output = np.zeros(self.y.shape)
 
-    def predict(self):
-        pass
+    def train(self):
+        self.__feedforward()
+        self.__backward_propogation()
+
+    def predict(self, x):
+        # TODO: is this correct? Probably not. Fix it.
+        self.input = x
+        self.__feedforward()
+        return self.output
+
+    def __feedforward(self):
+        self.layer1 = self.__sigmoid(np.dot(self.input, self.weights1))
+        self.output = self.__sigmoid(np.dot(self.layer1, self.weights2))
 
     def __backward_propogation(self):
         # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * __sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * __sigmoid_derivative(self.output), self.weights2.T) * __sigmoid_derivative(self.layer1)))
+        sse_derivative = 2*(self.y - self.output) 
+        loss_derivative = self.__sigmoid_derivative(self.output)
+
+        derivative_product = sse_derivative * loss_derivative
+
+        d_weights2 = np.dot(self.layer1.T, derivative_product)
+
+        layer2_gradient = np.dot(derivative_product, self.weights2.T) * self.__sigmoid_derivative(self.layer1)
+
+        d_weights1 = np.dot(self.input.T,  layer2_gradient)
 
         # update the weights with the derivative (slope) of the loss function
         self.weights1 += d_weights1
         self.weights2 += d_weights2
 
-    def __feedforward(self):
-        self.layer1 = __sigmoid(np.dot(self.input, self.weights1))
-        self.output = __sigmoid(np.dot(self.layer1, self.weights2))
-
     #https://stackoverflow.com/a/29863846/5217293
     def __sigmoid(self, x):
-        return math.exp(-np.logaddexp(0, -x))
+        return np.exp(-np.logaddexp(0, -x))
 
-    def __sigmoid_derivate(self, x):
+    def __sigmoid_derivative(self, x):
         return self.__sigmoid(x) * (1 - self.__sigmoid(x))
 
 if __name__ == '__main__':
-    ai = NeuralHandwritingNet()
-
     label_reader = LabelDataReader("training_data/train-labels-idx1-ubyte")
-    labels = label_reader.read()
+    labels = label_reader.read()[:1000]
 
     image_reader = ImageDataReader("training_data/train-images-idx3-ubyte")
-    images = image_reader.read()
+    images = image_reader.read()[:1000]
 
-    for (image, label) in tqdm(zip(images, labels), desc='Training'):
-        ai.train(image, label)
+    ai = NeuralHandwritingNet(images, labels)
+
+    for i in tqdm(range(1500), desc='Training'):
+        ai.train()
+
+    print(ai.output)
+
